32415cdbcd1f539dbc9940e6d1902711ecb32d8f
Nelson Patricio Jimenez <developer.jimenez@gmail.com>
2019-11-16 12:43:05 -0300

    I have issues to use sesion

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -5,12 +5,27 @@
     def sessions
       accounts = Spotify::Accounts.new
       url = accounts.authorize_url({
-        scope: "user-read-private user-read-email"
+        scope: %w(
+          user-read-private
+          user-read-email
+          user-read-recently-played
+          user-modify-playback-state
+          streaming
+          app-remote-control
+        ).join(' ')
       })
+
       redirect_to url
   end
 
   def redirect
     binding.pry
+    @session = accounts.exchange_for_session(accounts,params[:code])
+  end
+
+  private
+
+  def accounts
+    @accounts ||= Spotify::Accounts.new
   end
 end
